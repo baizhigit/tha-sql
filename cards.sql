9cf2ac2d25e0508c1f7fb2e780ba60d9779aaedf
Кобланды Байжигит <k.baizhigit@tha.kz>
2026-09-04 16:26:30 +0500

    card types 2

--- cards.sql
+++ cards.sql
@@ -31,7 +31,8 @@ SELECT
     ctc."concessionId",
     ctc."cardTypeId",
     ci.name AS city,
-    ct.name,
+    ct.name code,
+    tl.title_ru,
     rules.types AS type,
     rules.keys AS key,
     rules.values AS value,
@@ -42,6 +43,7 @@ SELECT
     cc.name AS category
 FROM public.card_type_concessions ctc
 LEFT JOIN public.card_types ct ON ct.id = ctc."cardTypeId"
+LEFT JOIN public.ct_card_titles tl on tl.nn = ct.name
 LEFT JOIN public.concessions c ON c.id = ctc."concessionId"
 LEFT JOIN public.concession_groups g ON g.id = c."groupId"
 LEFT JOIN (
