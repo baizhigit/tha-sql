85ddd90ed4790344e94d8f936d540e3deec22808
Кобланды Байжигит <k.baizhigit@tha.kz>
2026-09-04 15:58:00 +0500

    card types

--- cards.sql
+++ cards.sql
@@ -23,3 +23,37 @@ JOIN ct_card_tariffs AS tr
 CROSS JOIN LATERAL jsonb_each(tr.tariffs_json) AS j(key, value)
 -- WHERE t.nn = '60.50'
 ORDER BY city_id;
+
+
+
+SELECT 
+    ctc.id,
+    ctc."concessionId",
+    ctc."cardTypeId",
+    ci.name AS city,
+    ct.name,
+    rules.types AS type,
+    rules.keys AS key,
+    rules.values AS value,
+    c.name AS concession,
+    g.name AS groupName,
+    ct."isConcessionRequired" AS isConcession,
+    cs.name AS subcategory,
+    cc.name AS category
+FROM public.card_type_concessions ctc
+LEFT JOIN public.card_types ct ON ct.id = ctc."cardTypeId"
+LEFT JOIN public.concessions c ON c.id = ctc."concessionId"
+LEFT JOIN public.concession_groups g ON g.id = c."groupId"
+LEFT JOIN (
+    SELECT 
+        "concessionGroupId",
+        STRING_AGG(type, ', ' ORDER BY id) AS types,
+        STRING_AGG(key, ', ' ORDER BY id) AS keys,
+        STRING_AGG(value, ', ' ORDER BY id) AS values
+    FROM public.concession_rules
+    GROUP BY "concessionGroupId"
+) rules ON rules."concessionGroupId" = g.id
+LEFT JOIN public.concession_subcategories cs ON cs.id = c."subcategoryId"
+LEFT JOIN public.concession_categories cc ON cc.id = cs."concessionCategoryId"
+LEFT JOIN public.cities ci ON ci.id = cc."cityId";
+
